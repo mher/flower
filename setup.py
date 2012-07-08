f8c97e59a998d9b72c61e7f179f010dbf0f7a406
Mher Movsisyan <mher.movsisyan@gmail.com>
2012-07-08 17:28:59 +0400

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+version = re.compile(r'VERSION\s*=\s*\((.*?)\)')
+
+
+def get_package_version():
+    "returns package version without importing it"
+    base = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(base, "celeryadmin/__init__.py")) as initf:
+        for line in initf:
+            m = version.match(line.strip())
+            if not m:
+                continue
+            return ".".join(m.groups()[0].split(", "))
+
+
+setup(
+    name='celeryadmin',
+    version=get_package_version(),
+    description='Celery Web Admin',
+    long_description=open('README.rst').read(),
+    author='Mher Movsisyan',
+    url='https://github.com/mher/celery-admin',
+    packages=find_packages(),
+    install_requires=['celery', 'tornado'],
+    entry_points={
+        'console_scripts': [
+            'celeryadmin = celeryadmin.__main__:main',
+        ]
+    },
+)
