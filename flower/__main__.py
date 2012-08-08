1760997c030dda5a7322f96a95fd6fe3d5e440a3
Mher Movsisyan <mher.movsisyan@gmail.com>
2012-08-08 17:41:33 +0400

    Improves logging formatting

--- flower/__main__.py
+++ flower/__main__.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 
 import logging
 
+from pprint import pformat
+
 from tornado import ioloop
 from tornado.options import define, options, parse_command_line
 
@@ -15,7 +17,7 @@ from flower.settings import APP_SETTINGS
 
 define("port", default=5555, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode", type=bool)
-define("inspect", default=True, help="Inspect workers", type=bool)
+define("inspect", default=True, help="inspect workers", type=bool)
 
 
 def main(argv=None):
@@ -39,7 +41,7 @@ def main(argv=None):
 
     print('> Visit me at http://localhost:%s' % options.port)
 
-    logging.debug('Settings: %s' % APP_SETTINGS)
+    logging.debug('Settings: %s' % pformat(APP_SETTINGS))
 
     app.listen(options.port)
     try:
