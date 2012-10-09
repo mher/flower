57aeb0afaf00534c0a4d185cc04971ad38dae647
Gary Linscott <glinscott@gmail.com>
2012-10-09 16:12:05 -0400

    Fix errors

--- flower/api/workers.py
+++ flower/api/workers.py
@@ -1,13 +1,8 @@
 from __future__ import absolute_import
 
-import logging
-
-from tornado.escape import json_decode
 from tornado.web import RequestHandler, HTTPError
 
-from celery.result import AsyncResult
-from celery.backends.base import DisabledBackend
-
+from ..models import WorkersModel
 
 class ListWorkers(RequestHandler):
     def get(self):
