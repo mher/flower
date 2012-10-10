92a61de0c3759b4ba698e5e307c23e36b6ac57bb
Mher Movsisyan <mher.movsisyan@gmail.com>
2012-10-10 18:33:54 +0400

    Adds 404 response for API

--- flower/views/error.py
+++ flower/views/error.py
@@ -8,3 +8,6 @@ from ..views import BaseHandler
 class NotFoundErrorHandler(BaseHandler):
     def get(self):
         raise tornado.web.HTTPError(404)
+
+    def post(self):
+        raise tornado.web.HTTPError(404)
