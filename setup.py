7d4c846bbc174eb9ff86ff7a31f62f77835b7973
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-04-23 15:57:35 +0400

    Flower depends on redis

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     license='BSD',
     classifiers=classifiers,
     packages=find_packages(exclude=['tests', 'tests.*']),
-    install_requires=['celery', 'tornado', 'requests'],
+    install_requires=['celery', 'tornado', 'requests', 'redis'],
     package_data={'flower': ['templates/*', 'static/**/*']},
     entry_points={
         'console_scripts': [
