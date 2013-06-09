b21bc9541bc16d150935269743a2e90a7d8319ef
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-06-09 12:15:13 +0400

    Tests passing

--- flower/app.py
+++ flower/app.py
@@ -23,7 +23,7 @@ class Flower(tornado.web.Application):
         self.basic_auth = getattr(self.options, 'basic_auth', None)
         self.broker_api = getattr(self.options, 'broker_api', None)
         self.ssl = None
-        if self.options.certfile and self.options.keyfile:
+        if options and self.options.certfile and self.options.keyfile:
             cwd = os.environ.get('PWD') or os.getcwd()
             self.ssl = {
                 'certfile': os.path.join(cwd, self.options.certfile),
