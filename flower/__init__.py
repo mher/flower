8e42257877a9f822a26531a9e01a5ea3605a421a
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-11-10 21:43:03 +0400

    Bumps up develoment version to 0.7

--- flower/__init__.py
+++ flower/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-VERSION = (0, 6, 0)
-__version__ = '.'.join(map(str, VERSION))
+VERSION = (0, 7, 0)
+__version__ = '.'.join(map(str, VERSION)) + '-dev'
 
 from .app import Flower  # noqa
