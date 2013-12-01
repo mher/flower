27ad21bdca06390478e41aa925c0cb2eaa65f20c
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-12-01 21:05:15 +0400

    Refactor worker dashboard

--- flower/views/update.py
+++ flower/views/update.py
@@ -19,29 +19,24 @@ class UpdateWorkers(websocket.WebSocketHandler):
 
     def open(self):
         app = self.application
-        listeners = UpdateWorkers.listeners
-        periodic_callback = UpdateWorkers.periodic_callback
 
-        if not listeners:
+        if not self.listeners:
             logging.debug('Starting a timer for dashboard updates')
-            periodic_callback = periodic_callback or PeriodicCallback(
+            periodic_callback = self.periodic_callback or PeriodicCallback(
                 partial(UpdateWorkers.on_update_time, app),
                 PAGE_UPDATE_INTERVAL)
             if not periodic_callback._running:
                 periodic_callback.start()
-        listeners.append(self)
+        self.listeners.append(self)
 
     def on_message(self, message):
         pass
 
     def on_close(self):
-        listeners = UpdateWorkers.listeners
-        periodic_callback = UpdateWorkers.periodic_callback
-
-        listeners.remove(self)
-        if not listeners and periodic_callback:
+        self.listeners.remove(self)
+        if not self.listeners and self.periodic_callback:
             logging.debug('Stopping dashboard updates timer')
-            periodic_callback.stop()
+            self.periodic_callback.stop()
 
     @classmethod
     def on_update_time(cls, app):
