38dccd66557ae642fb6e876209a35c7d6f6709e9
Mher Movsisyan <mher.movsisyan@gmail.com>
2014-04-11 21:14:28 +0400

    Don't generate indexes

--- docs/conf.py
+++ docs/conf.py
@@ -156,10 +156,10 @@ html_static_path = ['_static']
 #html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_domain_indices = True
+html_domain_indices = False
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
