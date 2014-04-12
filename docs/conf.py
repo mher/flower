2b3b2f6c662eba6d3322e7c3631cdaf3b5c83784
Mher Movsisyan <mher.movsisyan@gmail.com>
2014-04-12 16:13:37 +0400

    Don't show page sources

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'flower'
+project = u'Flower'
 copyright = u'2014, Mher Movsisyan'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -165,7 +165,7 @@ html_use_index = False
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
