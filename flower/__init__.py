89cbf0adc7af51cb768a874f894e209ce1009b87
Mher Movsisyan <mher.movsisyan@gmail.com>
2014-05-25 18:32:32 +0400

    Set release version

--- flower/__init__.py
+++ flower/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
 VERSION = (0, 7, 0)
-__version__ = '.'.join(map(str, VERSION)) + '-dev'
+__version__ = '.'.join(map(str, VERSION))
 
 from .app import Flower  # noqa
