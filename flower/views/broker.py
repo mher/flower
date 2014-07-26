2c9bd583f8c969b1f1f5e20ac0f2039ffc3462e6
Mher Movsisyan <mher.movsisyan@gmail.com>
2014-07-26 21:49:40 +0400

    Warn about broker_api

--- flower/views/broker.py
+++ flower/views/broker.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import logging
+
 from tornado import web
 from tornado import gen
 
@@ -8,18 +10,28 @@ from ..utils.broker import Broker
 from ..api.control import ControlHandler
 
 
+logger = logging.getLogger(__name__)
+
+
 class BrokerView(BaseHandler):
     @web.authenticated
     @gen.coroutine
     def get(self):
-        app = self.application.celery_app
+        app = self.application
+        capp = app.celery_app
+
         if app.transport == 'amqp' and app.broker_api:
-            mgmnt_api = self.broker_api
+            mgmnt_api = app.broker_api
+        elif app.transport == 'amqp' and not app.broker_api:
+            logger.warning("Broker info is not available if --broker_api "
+                           "option is not configured. Make sure "
+                           "RabbitMQ Management Plugin is enabled ("
+                           "rabbitmq-plugins enable rabbitmq_management)")
         else:
             mgmnt_api = None
-        broker = Broker(app.connection().as_uri(include_password=True),
+        broker = Broker(capp.connection().as_uri(include_password=True),
                         mgmnt_api=mgmnt_api)
         queue_names = ControlHandler.get_active_queue_names()
         queues = yield broker.queues(queue_names)
-        self.render("broker.html", broker_url=app.connection().as_uri(),
+        self.render("broker.html", broker_url=capp.connection().as_uri(),
                                    queues=queues)
