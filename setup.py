de40e2be97b50cbc927acea072dd110815b74c49
Mher Movsisyan <mher.movsisyan@gmail.com>
2014-07-27 19:39:51 +0400

    Fix tornado requirement

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     license='BSD',
     classifiers=classifiers,
     packages=find_packages(exclude=['tests', 'tests.*']),
-    install_requires=['celery', 'tornado'],
+    install_requires=['celery', 'tornado>=3.2.0'],
     test_suite="tests",
     tests_require=['mock'],
     package_data={'flower': ['templates/*', 'static/**/*', 'static/*.*']},
