f6d0cbccddf111a723a371ec252e18e9b44c542f
mozillazg <opensource.mozillazg@gmail.com>
2015-01-15 15:16:28 +0800

    support sort tasks by name/state/recived/started #194

--- flower/utils/tasks.py
+++ flower/utils/tasks.py
@@ -1,9 +1,13 @@
 from celery.events.state import Task
 
 
-def iter_tasks(events, limit=None, type=None, worker=None, state=None):
+def iter_tasks(events, limit=None, type=None, worker=None, state=None,
+               sort_by=None):
     i = 0
-    for uuid, task in events.state.tasks_by_timestamp():
+    tasks = events.state.tasks_by_timestamp()
+    tasks = sort_tasks(tasks, sort_by)
+
+    for uuid, task in tasks:
         if type and task.name != type:
             continue
         if worker and task.worker and task.worker.hostname != worker:
@@ -14,6 +18,17 @@ def iter_tasks(events, limit=None, type=None, worker=None, state=None):
         i += 1
         if i == limit:
             break
+
+
+def sort_tasks(tasks, sort_by):
+    sorted_keys = ['name', 'state', 'received', 'started']
+    if sort_by and sort_by.lstrip('-') in sorted_keys:
+        if sort_by.startswith('-'):
+            sort_by = sort_by.lstrip('-')
+            tasks = sorted(tasks, key=lambda x: getattr(x[1], sort_by), reverse=True)
+        else:
+            tasks = sorted(tasks, key=lambda x: getattr(x[1], sort_by))
+    return tasks
 
 
 def get_task_by_id(events, task_id):
--- flower/views/tasks.py
+++ flower/views/tasks.py
@@ -33,19 +33,21 @@ class TasksView(BaseHandler):
         worker = self.get_argument('worker', None)
         type = self.get_argument('type', None)
         state = self.get_argument('state', None)
+        sort_by = self.get_argument('sort', None)
 
         worker = worker if worker != 'All' else None
         type = type if type != 'All' else None
         state = state if state != 'All' else None
 
         tasks = iter_tasks(app.events, limit=limit, type=type,
-                           worker=worker, state=state)
+                           worker=worker, state=state, sort_by=sort_by)
         tasks = imap(self.format_task, tasks)
         workers = app.events.state.workers
         seen_task_types = app.events.state.task_types()
         time = 'natural-time' if app.options.natural_time else 'time'
         if capp.conf.CELERY_TIMEZONE:
             time += '-' + capp.conf.CELERY_TIMEZONE
+        params = {k: v[-1] for k, v in self.request.query_arguments.items()}
 
         self.render("tasks.html", tasks=tasks,
                     task_types=seen_task_types,
@@ -55,7 +57,9 @@ class TasksView(BaseHandler):
                     worker=worker,
                     type=type,
                     state=state,
-                    time=time)
+                    time=time,
+                    sort_by=sort_by,
+                    params=params)
 
     def format_task(self, args):
         uuid, task = args
