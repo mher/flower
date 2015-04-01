90a12d8d289ae887ffe76ee3c796f90d6d867180
Ilya Georgievsky <i.georgievsky@drweb.com>
2015-04-01 15:23:12 +0300

    test_async_apply_wait added

--- tests/api/test_tasks.py
+++ tests/api/test_tasks.py
@@ -2,6 +2,7 @@ from mock import Mock
 from datetime import datetime, timedelta
 
 from celery.result import AsyncResult
+import celery.states as states
 
 from tests import AsyncHTTPTestCase
 
@@ -14,6 +15,29 @@ class AsyncApplyTests(AsyncHTTPTestCase):
 
         self.assertEqual(200, r.code)
         task.apply_async.assert_called_once_with(args=[], kwargs={})
+
+    def test_async_apply_wait(self):
+        from mock import patch, PropertyMock
+        import json
+
+        result = 'result'
+        with patch('celery.result.AsyncResult.state', new_callable=PropertyMock) as mock_state:
+            with patch('celery.result.AsyncResult.result', new_callable=PropertyMock) as mock_result:
+                mock_state.return_value = states.SUCCESS
+                mock_result.return_value = result
+
+                ar = AsyncResult(123)
+                ar.get = Mock(return_value=result)
+
+                task = self._app.capp.tasks['foo'] = Mock()
+                task.apply_async = Mock(return_value=ar)
+
+                r = self.post('/api/task/async-apply/foo', body='{"wait": true}')
+
+        self.assertEqual(200, r.code)
+        self.assertEqual(result, json.loads(r.body)['result'])
+        task.apply_async.assert_called_once_with(args=[], kwargs={}, wait=True)
+
 
     def test_async_apply_eta(self):
         task = self._app.capp.tasks['foo'] = Mock()
