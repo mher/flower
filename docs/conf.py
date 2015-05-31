4f71f5a5d7c185174eb870898dd1cf1811d70baf
Mher Movsisyan <mher.movsisyan@gmail.com>
2015-05-31 20:20:30 +0400

    Update copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Flower'
-copyright = u'2014, Mher Movsisyan'
+copyright = u'2015, Mher Movsisyan'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
