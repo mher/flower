537c892d392e86f33207795d062ad19dfa5ee03d
AlexV <asmodehn@gmail.com>
2015-07-18 17:28:20 +0900

    enabling cors by adding tornado cors mixin

--- flower/views/__init__.py
+++ flower/views/__init__.py
@@ -11,8 +11,37 @@ import tornado
 
 from ..utils import template, bugreport
 
+from tornado_cors import CorsMixin
 
-class BaseHandler(tornado.web.RequestHandler):
+class BaseHandler(CorsMixin, tornado.web.RequestHandler):
+
+    # Value for the Access-Control-Allow-Origin header.
+    # Default: None (no header).
+    CORS_ORIGIN = '*'
+
+    # Value for the Access-Control-Allow-Headers header.
+    # Default: None (no header).
+    CORS_HEADERS = 'Content-Type'
+
+    # Value for the Access-Control-Allow-Methods header.
+    # Default: Methods defined in handler class.
+    # None means no header.
+    CORS_METHODS = 'POST'
+
+    # Value for the Access-Control-Allow-Credentials header.
+    # Default: None (no header).
+    # None means no header.
+    CORS_CREDENTIALS = True
+
+    # Value for the Access-Control-Max-Age header.
+    # Default: 86400.
+    # None means no header.
+    CORS_MAX_AGE = 21600
+
+    # Value for the Access-Control-Expose-Headers header.
+    # Default: None
+    CORS_EXPOSE_HEADERS = 'Location, X-WP-TotalPages'
+
     def render(self, *args, **kwargs):
         functions = self._get_template_functions()
         assert not set(map(lambda x: x[0], functions)) & set(kwargs.keys())
