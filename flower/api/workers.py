9465fb738eeaac5f6166db047dcea4277b940619
Mher Movsisyan <mher.movsisyan@gmail.com>
2015-07-20 20:17:25 -0700

    Gracefully handle update errors ##434

--- flower/api/workers.py
+++ flower/api/workers.py
@@ -1,9 +1,14 @@
 from __future__ import absolute_import
 
+import logging
+
 from tornado import web
 from tornado import gen
 
 from .control import ControlHandler
+
+
+logger = logging.getLogger(__name__)
 
 
 class ListWorkers(ControlHandler):
@@ -61,7 +66,12 @@ List workers
             return
 
         if refresh:
-            yield self.update_cache(workername=workername)
+            try:
+                yield self.update_cache(workername=workername)
+            except Exception as e:
+                msg = "Failed to update workers: %s" % e
+                logger.error(msg)
+                raise web.HTTPError(503, msg)
 
         if workername and not self.is_worker(workername):
             raise web.HTTPError(404, "Unknown worker '%s'" % workername)
