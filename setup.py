febe1adc84a3d320a05113df31404b9e15d5182b
Florian Braun <florian.braun@paperc.de>
2016-01-14 14:59:34 +0100

    #511 fix setup.py

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     install_requires=install_requires,
     test_suite="tests",
     tests_require=get_requirements('test.txt'),
-    package_data={'flower': ['templates/*', 'static/**/*', 'static/*.*']},
+    package_data={'flower': ['templates/*', 'static/**/*', 'static/*.*', 'static/css/images/*.*']},
     entry_points={
         'console_scripts': [
             'flower = flower.__main__:main',
