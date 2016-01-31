c7ddf042cdfcc62c6b703402efa7204dd0920921
Mher Movsisyan <mher.movsisyan@gmail.com>
2016-01-30 18:00:26 -0800

    No need to sort workers

--- flower/views/dashboard.py
+++ flower/views/dashboard.py
@@ -42,8 +42,7 @@ class DashboardView(BaseHandler):
             info.update(self._as_dict(worker))
             info.update(status=worker.alive)
             workers[name] = info
-            
-        workers = OrderedDict(sorted(workers.items()))
+
         self.render("dashboard.html", workers=workers, broker=broker)
 
     @classmethod
