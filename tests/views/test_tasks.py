411f893194128e56ff6bf9c7e243eed4006e258a
Mher Movsisyan <mher.movsisyan@gmail.com>
2016-02-11 18:05:32 -0800

    Fix test_task_page test

--- tests/views/test_tasks.py
+++ tests/views/test_tasks.py
@@ -5,7 +5,7 @@ class TaskTest(AsyncHTTPTestCase):
     def test_task_page(self):
         r = self.get('/tasks')
         self.assertEqual(200, r.code)
-        self.assertTrue('Seen task types' in str(r.body))
+        self.assertTrue('Name' in str(r.body))
 
     def test_unknown_task(self):
         r = self.get('/task/unknown')
