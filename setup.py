2fcfa68383314c9eef495bd00155a3ba660280a6
Gavin Bisesi <gavin@curata.com>
2017-03-06 14:31:49 -0500

    Add setup.py troves for python 3.5, 3.6

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ classes = """
     Programming Language :: Python :: 3
     Programming Language :: Python :: 3.3
     Programming Language :: Python :: 3.4
+    Programming Language :: Python :: 3.5
+    Programming Language :: Python :: 3.6
     Programming Language :: Python :: Implementation :: CPython
     Programming Language :: Python :: Implementation :: PyPy
     Operating System :: OS Independent
