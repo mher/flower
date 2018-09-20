de0b7b0b9cbc445399d456a059abe4b02a1a0948
Pavel Savchenko <asfaltboy@gmail.com>
2018-09-20 17:56:52 +0200

    Document the GetQueueLengths API endpoint

--- flower/api/tasks.py
+++ flower/api/tasks.py
@@ -366,6 +366,36 @@ class GetQueueLengths(BaseTaskHandler):
     @web.authenticated
     @gen.coroutine
     def get(self):
+        """
+Return length of all active queues
+
+**Example request**:
+
+.. sourcecode:: http
+
+  GET /api/queues/length
+  Host: localhost:5555
+
+**Example response**:
+
+.. sourcecode:: http
+
+  HTTP/1.1 200 OK
+  Content-Length: 94
+  Content-Type: application/json; charset=UTF-8
+
+  {
+      "active_queues": [
+          {"name": "celery", "messages": 0},
+          {"name": "video-queue", "messages": 5}
+      ]
+  }
+
+:reqheader Authorization: optional OAuth token to authenticate
+:statuscode 200: no error
+:statuscode 401: unauthorized request
+:statuscode 503: result backend is not configured
+        """
         app = self.application
         broker_options = self.capp.conf.BROKER_TRANSPORT_OPTIONS
 
