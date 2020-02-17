ac7ff73fd6ed61f3162b24db61a2b3f1f60cf687
Alexandr Onufrienko <32614537+AlexandrOnuf@users.noreply.github.com>
2020-02-17 16:56:42 +0200

    [#712] fix sorting by runtime field runtime (#914)

--- flower/views/tasks.py
+++ flower/views/tasks.py
@@ -64,6 +64,8 @@ class TasksDataTable(BaseHandler):
         def key(item):
             return Comparable(getattr(item[1], sort_by))
 
+        self.maybe_normalize_for_sort(app.events.state.tasks_by_timestamp(), sort_by)
+
         sorted_tasks = sorted(
             iter_tasks(app.events, search=search),
             key=key,
@@ -82,6 +84,18 @@ class TasksDataTable(BaseHandler):
         self.write(dict(draw=draw, data=filtered_tasks,
                         recordsTotal=len(sorted_tasks),
                         recordsFiltered=len(sorted_tasks)))
+
+    @classmethod
+    def maybe_normalize_for_sort(cls, tasks, sort_by):
+        sort_keys = {'name': str, 'state': str, 'received': float, 'started': float, 'runtime': float}
+        if sort_by in sort_keys:
+            for _, task in tasks:
+                attr_value = getattr(task, sort_by, None)
+                if attr_value:
+                    try:
+                        setattr(task, sort_by, sort_keys[sort_by](attr_value))
+                    except TypeError:
+                        pass
 
     @web.authenticated
     def post(self):
