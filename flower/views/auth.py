b4e4e75cd4570c7a4c31b3ed544f3cee8a782bba
Paul Logston <paul.logston@gmail.com>
2020-03-13 17:03:47 +0800

    Add Okta Login Handler

--- flower/views/auth.py
+++ flower/views/auth.py
@@ -150,6 +150,87 @@ class GithubLoginHandler(BaseHandler, tornado.auth.OAuth2Mixin):
         self.redirect(next_)
 
 
+class OktaLoginHandler(BaseHandler, tornado.auth.OAuth2Mixin):
+
+    _OAUTH_AUTHORIZE_URL = "https://{okta_subdomain}/oauth2/v1/authorize"
+    _OAUTH_ACCESS_TOKEN_URL = "https://{okta_subdomain}/oauth2/v1/token"
+    _OAUTH_USER_INFO_URL = "https://{okta_subdomain}/oauth2/v1/userinfo"
+    _OAUTH_NO_CALLBACKS = False
+    _OAUTH_SETTINGS_KEY = 'oauth'
+
+    @tornado.gen.coroutine
+    def get_authenticated_user(self, redirect_uri, code):
+        body = urlencode({
+            "redirect_uri": redirect_uri,
+            "code": code,
+            "client_id": self.settings[self._OAUTH_SETTINGS_KEY]['key'],
+            "client_secret": self.settings[self._OAUTH_SETTINGS_KEY]['secret'],
+            "grant_type": "authorization_code",
+        })
+
+        response = yield self.get_auth_http_client().fetch(
+            self._OAUTH_ACCESS_TOKEN_URL,
+            method="POST",
+            headers={'Content-Type': 'application/x-www-form-urlencoded',
+                     'Accept': 'application/json'}, body=body)
+
+        if response.error:
+            raise tornado.auth.AuthError(
+                'OAuth authenticator error: %s' % str(response))
+
+        raise tornado.gen.Return(json.loads(response.body.decode('utf-8')))
+
+    @tornado.gen.coroutine
+    def get(self):
+        redirect_uri = self.settings[self._OAUTH_SETTINGS_KEY]['redirect_uri']
+        if self.get_argument('code', False):
+            user = yield self.get_authenticated_user(
+                redirect_uri=redirect_uri,
+                code=self.get_argument('code'),
+            )
+            yield self._on_auth(user)
+        else:
+            yield self.authorize_redirect(
+                redirect_uri=redirect_uri,
+                client_id=self.settings[self._OAUTH_SETTINGS_KEY]['key'],
+                scope=['email'],
+                response_type='code',
+                extra_params={'approval_prompt': 'auto'}
+            )
+
+    @tornado.gen.coroutine
+    def _on_auth(self, user):
+        if not user:
+            raise tornado.web.HTTPError(500, 'OAuth authentication failed')
+        access_token = user['access_token']
+
+        response = yield self.get_auth_http_client().fetch(
+            self._OAUTH_USER_INFO_URL,
+            headers={'Authorization': 'token ' + access_token,
+                     'User-agent': 'Tornado auth'})
+
+        decoded_body = json.loads(response.body.decode('utf-8'))
+        email = (decoded_body.get('email') or '').strip()
+        email_verified = (
+            decoded_body.get('email_verified') and
+            re.match(self.application.options.auth, email)
+        )
+
+        if not email_verified:
+            message = (
+                "Access denied. Please use another account or "
+                "ask your admin to add your email to flower --auth."
+            )
+            raise tornado.web.HTTPError(403, message)
+
+        self.set_secure_cookie("user", str(email))
+
+        next_ = self.get_argument('next', self.application.options.url_prefix or '/')
+        if self.application.options.url_prefix and next_[0] != '/':
+            next_ = '/' + next_
+        self.redirect(next_)
+
+
 class LogoutHandler(BaseHandler):
     def get(self):
         self.clear_cookie('user')
