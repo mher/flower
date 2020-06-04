0afeee3591f17a3f86b37db74d68a8d93c175272
mbierma <mbierma@users.noreply.github.com>
2020-06-03 23:08:46 -0700

    Update broker.py

--- flower/utils/broker.py
+++ flower/utils/broker.py
@@ -216,6 +216,26 @@ class RedisSocket(RedisBase):
         self.redis = redis.Redis(unix_socket_path='/' + self.vhost,
                                  password=self.password)
 
+        
+class RedisSsl(Redis):
+    """
+    Redis SSL class offering connection to the broker over SSL.
+    This does not currently support SSL settings through the url, only through
+    the broker_use_ssl celery configuration.
+    """
+
+    def __init__(self, broker_url, *args, **kwargs):
+        if 'broker_use_ssl' not in kwargs:
+            raise ValueError('rediss broker requires broker_use_ssl')
+        self.broker_use_ssl = kwargs.get('broker_use_ssl', {})
+        super(RedisSsl, self).__init__(broker_url, *args, **kwargs)
+
+    def _get_redis_client_args(self):
+        client_args = super(RedisSsl, self)._get_redis_client_args()
+        client_args['ssl'] = True
+        client_args.update(self.broker_use_ssl)
+        return client_args 
+
 
 class Broker(object):
     def __new__(cls, broker_url, *args, **kwargs):
@@ -224,6 +244,8 @@ class Broker(object):
             return RabbitMQ(broker_url, *args, **kwargs)
         elif scheme == 'redis':
             return Redis(broker_url, *args, **kwargs)
+        elif scheme == 'rediss':
+            return RedisSsl(broker_url, *args, **kwargs) 
         elif scheme == 'redis+socket':
             return RedisSocket(broker_url, *args, **kwargs)
         elif scheme == 'sentinel':
