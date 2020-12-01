b004231bb5320238e591cb1e6bcde35c59fd40ff
Christoph Buelter <christoph.buelter@myshopradio.de>
2020-12-01 14:34:39 +0100

    Do not fail filtering by args:foo if task.args is None

--- flower/utils/search.py
+++ flower/utils/search.py
@@ -80,4 +80,6 @@ def preprocess_search_value(raw_value):
 
 
 def task_args_contains_search_args(task_args, search_args):
+    if not task_args:
+        return False
     return all(a in task_args for a in search_args)
