21cda2ef8f4c4b380d225ceec60102f4f97703ad
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2021-01-16 23:21:29 +0300

    Remove deprecated Python version (3.5)

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ classes = """
     Programming Language :: Python
     Programming Language :: Python :: 3
     Programming Language :: Python :: 3 :: Only
-    Programming Language :: Python :: 3.5
     Programming Language :: Python :: 3.6
     Programming Language :: Python :: 3.7
     Programming Language :: Python :: 3.8
