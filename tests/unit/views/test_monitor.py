c7359f052fb877ff318599a364530c928d4461ba
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2021-06-22 00:46:09 +0100

    Add number of tasks queuing at worker Prometheus metric (#1117)
    * Add number_of_tasks_queuing_at_worker metric.
    
    This will show number of tasks fetched to the worker. Since Celery fetches prefetch_multiplier x concurrency setting number of tasks this will effectively cap out at that number.
    This is useful to see if we hit the the cap and have to upgrade the hardware horizontally or vertically.
    
    Fix last recorded task queuing time being used forever.
    
    In case if a given worker recorded some queuing time for task with task name and then we never received any more tasks with that name on that worker the last recorded queuing time
    would be used forever and the graph would never go back to zero.
    Now if the task succeeds or fails it resets the queuing time.
    
    * Extract queuing time metrics tests.
    Add tests for number of tasks queuing at worker.
    
    * Update docs on new Prometheus metric.
    
    * Fix tests.
    
    * Adjust naming to match Celery's way.
    
    Celery project uses `prefetch` instead of queuing when talking about tasks pre-downloaded to the worker to be ready for execution when the previous task finishes.
    This change keeps it in line with their terminology.
    
    * Replace grafana-dashboard.png with a new one showing more metrics.
    
    Add screenshot of configuring celery monitoring dashboard with Prometheus as data source.
    
    * Update documentation about new metrics.
    
    Add screenshot showing setting up grafana dashboard with prometheus data source.
    
    * update grafana dashboard example - this time it contains the data source too (and all the new panels).
    
    * Update prometheus alert examples to use new metric names.

--- tests/unit/views/test_monitor.py
+++ tests/unit/views/test_monitor.py
@@ -3,8 +3,10 @@ import time
 from datetime import datetime, timedelta
 
 from celery.events import Event
+from kombu import uuid
+
 from flower.events import EventsState
-from tests.unit.utils import task_succeeded_events
+from tests.unit.utils import task_succeeded_events, task_failed_events
 from tests.unit import AsyncHTTPTestCase
 
 
@@ -26,6 +28,29 @@ class PrometheusTests(AsyncHTTPTestCase):
         ]
         events += task_succeeded_events(worker=worker_name, name=task_name, id='123')
 
+        for i, e in enumerate(events):
+            e['clock'] = i
+            e['local_received'] = time.time()
+            state.event(e)
+        self.app.events.state = state
+
+        metrics = self.get('/metrics').body.decode('utf-8')
+        events = dict(re.findall('flower_events_total{task="task1",type="(task-.*)",worker="worker1"} (.*)', metrics))
+
+        self.assertTrue('task-received' in events)
+        self.assertTrue('task-started' in events)
+        self.assertTrue('task-succeeded' in events)
+
+        self.assertTrue(f'flower_worker_online{{worker="{worker_name}"}} 1.0' in metrics)
+        self.assertTrue(f'flower_worker_number_of_currently_executing_tasks{{worker="{worker_name}"}} 1.0' in metrics)
+
+    def test_task_prefetch_time_metric(self):
+        state = EventsState()
+        worker_name = 'worker1'
+        task_name = 'task1'
+        state.get_or_create_worker(worker_name)
+        events = task_succeeded_events(worker=worker_name, name=task_name, id='123')[:-1]
+
         task_received = time.time()
         task_started = task_received + 3
         for i, e in enumerate(events):
@@ -39,19 +64,62 @@ class PrometheusTests(AsyncHTTPTestCase):
         self.app.events.state = state
 
         metrics = self.get('/metrics').body.decode('utf-8')
-        events = dict(re.findall('flower_events_total{task="task1",type="(task-.*)",worker="worker1"} (.*)', metrics))
 
-        self.assertTrue('task-received' in events)
-        self.assertTrue('task-started' in events)
-        self.assertTrue('task-succeeded' in events)
         self.assertTrue(
-            f'flower_task_queuing_time_at_worker_seconds{{task="{task_name}",worker="{worker_name}"}} 3.0' in metrics
+            f'flower_task_prefetch_time_seconds{{task="{task_name}",worker="{worker_name}"}} 3.0' in metrics
         )
 
-        self.assertTrue(f'flower_worker_online{{worker="{worker_name}"}} 1.0' in metrics)
-        self.assertTrue(f'flower_worker_number_of_currently_executing_tasks{{worker="{worker_name}"}} 1.0' in metrics)
+    def test_task_prefetch_time_metric_successful_task_resets_metric_to_zero(self):
+        state = EventsState()
+        worker_name = 'worker1'
+        task_name = 'task1'
+        state.get_or_create_worker(worker_name)
+        events = task_succeeded_events(worker=worker_name, name=task_name, id='123')
 
-    def test_does_not_compute_queuing_time_if_task_has_eta(self):
+        task_received = time.time()
+        task_started = task_received + 3
+        for i, e in enumerate(events):
+            e['clock'] = i
+            e['local_received'] = time.time()
+            if e['type'] == 'task-received':
+                e['timestamp'] = task_received
+            if e['type'] == 'task-started':
+                e['timestamp'] = task_started
+            state.event(e)
+        self.app.events.state = state
+
+        metrics = self.get('/metrics').body.decode('utf-8')
+
+        self.assertTrue(
+            f'flower_task_prefetch_time_seconds{{task="{task_name}",worker="{worker_name}"}} 0.0' in metrics
+        )
+
+    def test_task_prefetch_time_metric_failed_task_resets_metric_to_zero(self):
+        state = EventsState()
+        worker_name = 'worker1'
+        task_name = 'task1'
+        state.get_or_create_worker(worker_name)
+        events = task_failed_events(worker=worker_name, name=task_name, id='123')
+
+        task_received = time.time()
+        task_started = task_received + 3
+        for i, e in enumerate(events):
+            e['clock'] = i
+            e['local_received'] = time.time()
+            if e['type'] == 'task-received':
+                e['timestamp'] = task_received
+            if e['type'] == 'task-started':
+                e['timestamp'] = task_started
+            state.event(e)
+        self.app.events.state = state
+
+        metrics = self.get('/metrics').body.decode('utf-8')
+
+        self.assertTrue(
+            f'flower_task_prefetch_time_seconds{{task="{task_name}",worker="{worker_name}"}} 0.0' in metrics
+        )
+
+    def test_task_prefetch_time_metric_does_not_compute_prefetch_time_if_task_has_eta(self):
         state = EventsState()
         worker_name = 'worker2'
         task_name = 'task2'
@@ -69,7 +137,7 @@ class PrometheusTests(AsyncHTTPTestCase):
         metrics = self.get('/metrics').body.decode('utf-8')
 
         self.assertFalse(
-            f'flower_task_queuing_time_at_worker_seconds{{task="{task_name}",worker="{worker_name}"}} ' in metrics
+            f'flower_task_prefetch_time_seconds{{task="{task_name}",worker="{worker_name}"}} ' in metrics
         )
 
     def test_worker_online_metric_worker_is_offline(self):
@@ -86,6 +154,48 @@ class PrometheusTests(AsyncHTTPTestCase):
         metrics = self.get('/metrics').body.decode('utf-8')
 
         self.assertTrue(f'flower_worker_online{{worker="{worker_name}"}} 0.0' in metrics)
+
+    def test_worker_prefetched_tasks_metric(self):
+        state = EventsState()
+        worker_name = 'worker2'
+        task_name = 'task1'
+        task_id = uuid()
+        state.get_or_create_worker(worker_name)
+        events = [
+            Event(
+                'task-received',
+                uuid=task_id,
+                name=task_name,
+                args='(2, 2)',
+                kwargs="{'foo': 'bar'}",
+                retries=1,
+                eta=None,
+                hostname=worker_name
+            ),
+            Event(
+                'task-received',
+                uuid=uuid(),
+                name=task_name,
+                args='(2, 2)',
+                kwargs="{'foo': 'bar'}",
+                retries=1,
+                eta=None,
+                hostname=worker_name
+            ),
+            Event('task-started', uuid=task_id, hostname=worker_name),
+        ]
+
+        for i, e in enumerate(events):
+            e['clock'] = i
+            e['local_received'] = time.time()
+            state.event(e)
+        self.app.events.state = state
+
+        metrics = self.get('/metrics').body.decode('utf-8')
+
+        self.assertTrue(
+            f'flower_worker_prefetched_tasks{{task="{task_name}",worker="{worker_name}"}} 1.0' in metrics
+        )
 
 
 class HealthcheckTests(AsyncHTTPTestCase):
