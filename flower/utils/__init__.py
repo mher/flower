mher/flower

commit 70b1ae40b467a3febab9abef3ef52dea56ca6f12

Fix attribute error caused by newest humanize package. (#1156)
When accessing humanize.VERSION we were running into an attribute error since they removed VERSION from that file.
We are supposed to use the standard __version__ now.

Also set humanize package version in stone.
Modify message from bugreport function.
Add test confirming new bugreport behaviour.

flower/utils/__init__.py
@@ -20,11 +20,11 @@ def bugreport(app=None):
         return 'flower   -> flower:%s tornado:%s humanize:%s%s' % (
             __version__,
             tornado.version,
-            humanize.VERSION,
+            humanize.__version__,
             app.bugreport()
         )
-    except (ImportError, AttributeError):
-        return 'Unknown Celery version'
+    except (ImportError, AttributeError) as e:
+        return f"Error when generating bug report: {e}. Have you installed correct versions of Flower's dependencies?"
 
 
 def abs_path(path):

tests/unit/utils/test_utils.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock
 
 from flower.utils import bugreport
 from celery import Celery
@@ -7,7 +8,7 @@ from celery import Celery
 class BugreportTests(unittest.TestCase):
     def test_default(self):
         report = bugreport()
-        self.assertFalse('Unknown Celery version' in report)
+        self.assertFalse('Error when generating bug report' in report)
         self.assertTrue('tornado' in report)
         self.assertTrue('humanize' in report)
         self.assertTrue('celery' in report)
@@ -15,7 +16,15 @@ class BugreportTests(unittest.TestCase):
     def test_with_app(self):
         app = Celery()
         report = bugreport(app)
-        self.assertFalse('Unknown Celery version' in report)
+        self.assertFalse('Error when generating bug report' in report)
         self.assertTrue('tornado' in report)
         self.assertTrue('humanize' in report)
         self.assertTrue('celery' in report)
+
+    def test_when_unable_to_generate_report(self):
+        fake_app = Mock()
+        fake_app.bugreport.side_effect = ImportError('import error message')
+        report = bugreport(fake_app)
+        self.assertTrue('Error when generating bug report' in report)
+        self.assertTrue('import error message' in report)
+        self.assertTrue("Have you installed correct versions of Flower's dependencies?" in report)

commit 909f19873889d7f3859e9d08285ff0f7665660cf

Add compatibility with older humanize versions
Also drop the humanize pinning since newer versions work too.

Fixes: commit 70b1ae40b467a3febab9abef3ef52dea56ca6f12
See-also: https://github.com/mher/flower/pull/1156

flower/utils/__init__.py
@@ -20,7 +20,7 @@ def bugreport(app=None):
         return 'flower   -> flower:%s tornado:%s humanize:%s%s' % (
             __version__,
             tornado.version,
-            humanize.__version__,
+            getattr(humanize, '__version__', getattr(humanize, 'VERSION'))
             app.bugreport()
         )
     except (ImportError, AttributeError) as e: