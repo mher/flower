719490a65fc65e27e7bd7421a31313cf097f54c7
Tanner Prynn <1757689+tprynn@users.noreply.github.com>
2022-04-23 16:19:51 -0500

    Remove extraneous CORS headers
    These CORS headers set here are not needed and ineffective - the specific methods which are allowed are not actually used in the app, so these will have no effect. CORS headers should not be set without an explicit purpose, because it could introduce additional vulnerabilities.

--- flower/views/__init__.py
+++ flower/views/__init__.py
@@ -15,12 +15,6 @@ logger = logging.getLogger(__name__)
 
 
 class BaseHandler(tornado.web.RequestHandler):
-    def set_default_headers(self):
-        self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-        self.set_header('Access-Control-Allow-Methods',
-                        ' PUT, DELETE, OPTIONS')
-
     def options(self):
         self.set_status(204)
         self.finish()
