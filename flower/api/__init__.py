eede7bb82355f20bb43d9b6ef1bc7dc83de0275d
Mher Movsisyan <mher@users.noreply.github.com>
2022-06-13 23:23:42 -0400

    Depricate WebSocket API

--- flower/api/__init__.py
+++ flower/api/__init__.py
@@ -1,28 +1,5 @@
-import tornado.websocket
+import tornado.web
 from ..views import BaseHandler
-
-class BaseWebSocketHandler(tornado.websocket.WebSocketHandler):
-    # listeners = [], should be created in derived class
-
-    def open(self):
-        listeners = self.listeners
-        listeners.append(self)
-
-    def on_message(self, message):
-        pass
-
-    def on_close(self):
-        listeners = self.listeners
-        if self in listeners:
-            listeners.remove(self)
-
-    @classmethod
-    def send_message(cls, message):
-        for l in cls.listeners:
-            l.write_message(message)
-
-    def check_origin(self, origin):
-        return True
 
 class BaseApiHandler(BaseHandler):
     def prepare(self):
