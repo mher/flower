5ca140452a76e1ec61cbfe7fff94f55c27c40804
Amit Phulera <amitphulera999@gmail.com>
2022-09-08 13:32:01 +0530

    remove the test as now celery will not return unsafe strings

--- tests/unit/api/test_workers.py
+++ tests/unit/api/test_workers.py
@@ -17,29 +17,11 @@ empty_inspect_response = {
     'celery@worker1': []
 }
 
-inspect_response_with_non_serializable_args = {
-    'celery@worker1': {
-        'active': {
-            'id': '1234',
-            'args': [set([1, 2])]
-        }
-    }
-}
-
 
 @mock.patch.object(Inspector, 'methods',
                    new_callable=mock.PropertyMock,
                    return_value=['inspect_method'])
 class ListWorkersTest(AsyncHTTPTestCase):
-
-    def test_non_serialisable_args_in_response(self, m_inspect):
-        celery = self._app.capp
-        celery.control.inspect = mock.Mock()
-        celery.control.inspect.return_value.inspect_method = mock.Mock(
-            return_value=inspect_response_with_non_serializable_args
-        )
-        # this will fail with a response code of 500
-        self.get('/api/workers?refresh=1')
 
     def test_refresh_cache(self, m_inspect):
         celery = self._app.capp
