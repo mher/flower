42515438b326113590d99aba05f79c81fd74baab
Sergey Klyuykov <onegreyonewhite@mail.ru>
2022-11-30 17:59:57 +0400

    Fix: Removed deprecated distutils logic and add test for latest python versions.

--- flower/views/__init__.py
+++ flower/views/__init__.py
@@ -4,7 +4,6 @@ import traceback
 import copy
 import logging
 
-from distutils.util import strtobool
 from base64 import b64decode
 
 import tornado
@@ -12,6 +11,22 @@ import tornado
 from ..utils import template, bugreport, prepend_url
 
 logger = logging.getLogger(__name__)
+
+
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
 
 
 class BaseHandler(tornado.web.RequestHandler):
