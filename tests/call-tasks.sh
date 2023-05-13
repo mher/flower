55558ba639433bada90a6d505f81ab7eb167aff9
Mher Movsisyan <mher@users.noreply.github.com>
2023-05-13 23:04:04 +0400

    Fix call-tasks script

--- tests/call-tasks.sh
+++ tests/call-tasks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-ROOT_DIR="git rev-parse --show-toplevel"
+export ROOT_DIR=$(git rev-parse --show-toplevel)
 export PYTHONPATH="$PYTHONPATH:$ROOT_DIR/examples"
 
 N=${1:-10}
@@ -10,5 +10,5 @@ for i in $(seq 1 $N); do
     celery -A tasks call tasks.add --args="[$i,$i]";
 done
 
-celery -A tasks call tasks.sleep --args='[10]'
+celery -A tasks call tasks.sleep --args='[100]'
 celery -A tasks call tasks.error --args='[10]'
