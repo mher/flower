6297d73312bd68688e00f861608ecb8468933674
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-11-10 02:25:50 +0300

    Remove Python 3.8 support (EOL)

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ classes = """
     Programming Language :: Python
     Programming Language :: Python :: 3
     Programming Language :: Python :: 3 :: Only
-    Programming Language :: Python :: 3.8
     Programming Language :: Python :: 3.9
     Programming Language :: Python :: 3.10
     Programming Language :: Python :: 3.11
